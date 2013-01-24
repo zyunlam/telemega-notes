6adf9d2a17d701ed0ceb742f322fa72723149980
Keith Packard <keithp@keithp.com>
2013-01-23 21:37:24 -0800

    altos/fox: Turn on all of the fox lights
    Fox has two red and two green LEDs
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/fox1ihu/ao_fox1ihu.c
+++ src/fox1ihu/ao_fox1ihu.c
@@ -31,7 +31,7 @@ main(void)
 
 	ao_task_init();
 	ao_led_init(LEDS_AVAILABLE);
-	ao_led_on(AO_LED_RED);
+	ao_led_on(AO_LED_RED|AO_LED_GREEN|AO_LED_RED_2|AO_LED_GREEN_2);
 	ao_timer_init();
 
 	ao_spi_init();
