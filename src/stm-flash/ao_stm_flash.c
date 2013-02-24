5187bb4402874f654fcef5ffdf237edf97a26b8c
Keith Packard <keithp@keithp.com>
2013-02-24 00:18:14 -0800

    Add STM self-flashing loader
    This allows the real application to get loaded at 0x2000 and jumps to
    that at startup time if the boot pin is set appropriately
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ src/stm-flash/ao_stm_flash.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2011 Keith Packard <keithp@keithp.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+#include "ao.h"
+#include <ao_exti.h>
+
+void
+ao_panic(uint8_t c)
+{
+}
+
+void
+ao_test(void)
+{
+	char	c;
+
+	for (;;) {
+		c = ao_usb_getchar();
+		ao_usb_putchar(c);
+		ao_usb_flush();
+	}
+}
+
+struct ao_task ao_test_task;
+
+int
+main(void)
+{
+	ao_clock_init();
+	ao_timer_init();
+//	ao_dma_init();
+	ao_cmd_init();
+//	ao_exti_init();
+	ao_usb_init();
+
+	ao_start_scheduler();
+	return 0;
+}
