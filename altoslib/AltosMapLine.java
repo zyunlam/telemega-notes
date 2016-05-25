7b5521966119fcc290591bf1b397506ef44cedea
Keith Packard <keithp@keithp.com>
2016-05-24 23:40:03 -0700

    altoslib: use miles for distances > 1000ft.
    This makes both the map line and the other distance displays use miles
    for distances greater than 1000 feet.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- altoslib/AltosMapLine.java
+++ altoslib/AltosMapLine.java
@@ -50,7 +50,7 @@ public abstract class AltosMapLine {
 
 		if (AltosConvert.imperial_units) {
 			distance = AltosConvert.meters_to_feet(distance);
-			if (distance < 10000) {
+			if (distance < 1000) {
 				format = "%4.0fft";
 			} else {
 				distance /= 5280;
@@ -64,7 +64,7 @@ public abstract class AltosMapLine {
 					format = "%5.0fmi";
 			}
 		} else {
-			if (distance < 10000) {
+			if (distance < 1000) {
 				format = "%4.0fm";
 			} else {
 				distance /= 1000;
