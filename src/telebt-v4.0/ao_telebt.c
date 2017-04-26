1f723d31312be6dea3ef46e265c17c4d361d7099
Bdale Garbee <bdale@gag.com>
2017-04-25 22:10:41 -0600

    telebt-v4.0: add ao_send_packet

--- src/telebt-v4.0/ao_telebt.c
+++ src/telebt-v4.0/ao_telebt.c
@@ -19,6 +19,7 @@
 #include <ao.h>
 #include <ao_exti.h>
 #include <ao_packet.h>
+#include <ao_send_packet.h>
 
 int
 main(void)
@@ -36,6 +37,7 @@ main(void)
 
 	ao_adc_init();
 	ao_cmd_init();
+	ao_send_packet_init();
 
 	ao_usb_init();
 	ao_radio_init();
