db7308750b3478ec3d2332e81db9957a9d4f2cfc
Keith Packard <keithp@keithp.com>
2019-01-28 11:35:32 -0800

    altos/telefireeight-v2.0: Add flash loader Makefile
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/telefireeight-v2.0/flash-loader/ao_pins.h
+++ src/telefireeight-v2.0/flash-loader/ao_pins.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Keith Packard <keithp@keithp.com>
+ * Copyright © 2019 Keith Packard <keithp@keithp.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
