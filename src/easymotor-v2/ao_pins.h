f5abe3bfaa2db1bf2fe67954ba90e414dd5ad72b
Keith Packard <keithp@keithp.com>
2020-10-10 21:56:38 -0700

    altos/easymotor-v2: Add 'through' axis definition
    This is used by the ao_flight_test_code
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/easymotor-v2/ao_pins.h
+++ src/easymotor-v2/ao_pins.h
@@ -157,6 +157,7 @@ struct ao_adc {
 
 #define AO_ADXL375_AXIS		x
 #define AO_ADXL375_ACROSS_AXIS	y
+#define AO_ADXL375_THROUGH_AXIS	z
 #define AO_ADXL375_INVERT	0
 #define HAS_IMU			1
 #define USE_ADXL375_IMU		1
