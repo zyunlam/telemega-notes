b4fa98e69931126ab01f85072ef26cdb9fdf7377
Keith Packard <keithp@keithp.com>
2020-10-16 12:58:57 -0700

    altos: Shorten accel-only integration ground interval
    Set interval to 64 samples for resetting the accumulated speed/height
    on the ground in accel-only configurations.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/kernel/ao_sample.c
+++ src/kernel/ao_sample.c
@@ -323,7 +323,7 @@ ao_sample_preflight_update(void)
 	else
 		ao_sample_preflight_set();
 #if !HAS_BARO
-	if ((nsamples & 0x7f) == 0)
+	if ((nsamples & 0x3f) == 0)
 		ao_kalman_reset_accumulate();
 #endif
 }
