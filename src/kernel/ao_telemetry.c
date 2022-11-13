4ff9f3271f9d20603626c18ee16b20e50c579ddf
Keith Packard <keithp@keithp.com>
2022-11-12 23:37:29 -0800

    altos: Make sure APRS packets are reasonably separated
    Don't let them come too close together.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/kernel/ao_telemetry.c
+++ src/kernel/ao_telemetry.c
@@ -490,7 +490,10 @@ ao_set_aprs_time(void)
 		} else {
 			delta = second - ao_gps_data.second;
 		}
-		ao_aprs_time = ao_gps_tick + AO_SEC_TO_TICKS(delta);
+		if (delta < (interval >> 1))
+			delta += interval;
+
+		ao_aprs_time = ao_gps_utc_tick + AO_SEC_TO_TICKS(delta);
 	} else {
 		ao_aprs_time += AO_SEC_TO_TICKS(ao_config.aprs_interval);
 	}
